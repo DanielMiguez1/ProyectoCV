49630a5f89d783a6e55e605ff98a6f15451934c6
DanielMiguez1 <126735958+DanielMiguez1@users.noreply.github.com>
2023-03-01 23:02:59 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,10 @@
+
+let mostrarTextoBtn = document.getElementById ('mostrarTextoBtn');
+
+let ocultarTexto = document.getElementById ('ocultarTexto');
+
+mostrarTextoBtn.addEventListener ('click', mostrarTexto);
+
+function mostrarTexto() {  
+    ocultarTexto.classList.mostrarTexto(verTexto)
+}
